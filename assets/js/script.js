725cc84b87e342b9d98ae08b85e0ea71143cef64
Marco Orazio Re <marcooraziore@gmail.com>
2021-04-19 15:39:25 +0200

    Milestone #1
    - Now when the search button is clicked you call the search film API on console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
 function init() {
     console.log('hello world');
     new Vue ({
+        el: '#app',
 
+        data: {
+            searchFilm: ''
+        },
+
+        methods: {
+            getFilms: function () {
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+
+                    params: {
+                        'api_key': '4112d8611cb3fa646e80b753d8213869',
+                        'query':'ritorno al futuro'
+                    }
+                })
+                .then(data => {console.log(data);})
+            }
+        },
+
+        mounted() {            
+        },
+
+        computed: {
+
+        }
     });
 }
 
