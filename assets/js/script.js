5d753a5967b5f48ddca38b7e1d00f9485d834ce4
Marco Orazio Re <marcooraziore@gmail.com>
2021-04-19 17:47:12 +0200

    Milestone #2.2
    - Now tv series are displayed on screen when you search on search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 function init() {
-    console.log('hello world');
+
     new Vue ({
         el: '#app',
 
         data: {
-            searchFilm: '',
+            search: '',
             films: [],
+            tvSeries: [],
             flags: {
                 ita: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg',
                 en : 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Flag_of_the_United_Kingdom.svg',
@@ -19,12 +20,26 @@ function init() {
 
                     params: {
                         'api_key': '4112d8611cb3fa646e80b753d8213869',
-                        'query': this.searchFilm
+                        'query': this.search
                     }
                 })
                 .then(data => {
                     this.films = data.data.results;
                     console.log(this.films);
+                })
+            },
+
+            getTvSeries: function () {
+                axios.get('https://api.themoviedb.org/3/search/tv', {
+
+                    params: {
+                        'api_key': '4112d8611cb3fa646e80b753d8213869',
+                        'query': this.search
+                    }
+                })
+                .then(data => {
+                    this.tvSeries = data.data.results;
+                    console.log(this.tvSeries);
                 })
             }
         },
