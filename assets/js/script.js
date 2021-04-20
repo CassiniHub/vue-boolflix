3fd653ba1900eba617d1096284ae30614627d15d
Marco Orazio Re <marcooraziore@gmail.com>
2021-04-20 15:56:44 +0200

    Milestone #3
    - Now the language flag is taken through a function using a switch

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,13 @@ function init() {
         data: {
             search: 'ritorno al fut',
             films: [],
-            tvSeries: [],
-            flags: {
-                ita: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg',
-                en : 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Flag_of_the_United_Kingdom.svg',
-                unk: 'https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg'
-            }
+            tvSeries: []
+            // Ver #1 img poster
+            // flags: {
+            //     ita: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg',
+            //     en : 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Flag_of_the_United_Kingdom.svg',
+            //     unk: 'https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg'
+            // }
         },
 
         methods: {
@@ -43,7 +44,29 @@ function init() {
                 })
             },
 
+            getLanguageFlag: function (elem) {
+                let urlImg = '';
+                let language = elem.original_language;
+
+                switch (true) {
+                    case language === 'en':
+                        urlImg = 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Flag_of_the_United_Kingdom.svg';
+                        break;
+                    
+                    case language === 'it':
+                        urlImg = 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg';
+                        break;
+
+                    default:
+                        urlImg = 'https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg';
+                        break;
+                }
+
+                return urlImg;
+            },
+
             getStarRange: function (array) {
+                // creating needed parameters to calculate the new rating range (from 0 / 10 to 0 / 5)
                 const oldMin = 0;
                 const oldMax = 10;
                 const newMin = 0;
@@ -52,15 +75,19 @@ function init() {
                 for (let i = 0; i < array.length; i++) {
                     const elem = array[i];
                     
+                    // get the old value
                     const oldValue = elem.vote_average;
 
+                    // get the new value
                     const newValue = Math.round((((oldValue - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin);
 
+                    // change the old value inside the array (films / tvSeries) with the new value
                     elem.vote_average = newValue;
                     console.log(elem.vote_average);
                 }
             },
 
+            // 
             starsRating: function (elem) {
                 return elem.vote_average * 20;
             }
