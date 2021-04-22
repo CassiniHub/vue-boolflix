ceef34aeb7cb9f69dc0bc92ac7f1c7e2c363a57a
Marco Orazio Re <marcooraziore@gmail.com>
2021-04-22 21:24:40 +0200

    Milestone #5 - After review
    - Refactoring of the getLanguageFlag and getCast functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,17 +39,9 @@ function init() {
                         }
                     })
                     .then(data => {
-                        let actors = [...data.data.cast];
-                        actors.splice(5);
                         
-                        // Create the object of the selected film
-                        let filmCast = {
-                            id: id,
-                            actors: actors
-                        }
-                        
-                        // Link the created object to the data one
-                        this.filmCast = filmCast;
+                        // link the created cast object to the data variable used in html
+                        this.filmCast = this.getSelectedCast([...data.data.cast], id);
                     })
             },
 
@@ -76,23 +68,42 @@ function init() {
                         }
                     })
                     .then(data => {
-                        let actors = [...data.data.cast];
-                        actors.splice(5);
                         
-                        // Create the object of the selected tv series
-                        let tvSeriesCast = {
-                            id: id,
-                            actors: actors
-                        }
-
-                        // Link the created object to the data one
-                        this.tvSeriesCast = tvSeriesCast;
+                        // link the created cast object to the data variable used in html
+                        this.tvSeriesCast = this.getSelectedCast([...data.data.cast], id);
                     })
+            },
+
+            // Create the object of the selected film/tv series cast
+            getSelectedCast: function (arrayCast, id) {
+                let actors = arrayCast;
+                    actors.splice(5);
+                    
+                    // Create the object of the selected tv series
+                    let cast = {
+                        id: id,
+                        actors: actors
+                    }
+
+                    return cast;
             },
 
             // Get % of the full stars to show in html
             starsRating: function (elem) {
                 return elem.vote_average * 20;
+            },
+
+            // Add flag key to an object
+            getLanguageFlag: function (obj) {
+                if (obj.original_language == 'en') {
+                    obj.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Flag_of_the_United_Kingdom.svg';
+
+                } else if (obj.original_language == 'it') {
+                    obj.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg';
+
+                } else {
+                    obj.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg';
+                }
             }
         },
 
@@ -105,17 +116,9 @@ function init() {
 
                     // Create a copy of every film object to manipulate without modifying the original object
                     let newFilm = {...film}
-
+                    
                     // Add flag key to every film object
-                    if (newFilm.original_language == 'en') {
-                        newFilm.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Flag_of_the_United_Kingdom.svg';
-
-                    } else if (newFilm.original_language == 'it') {
-                        newFilm.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg';
-
-                    } else {
-                        newFilm.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg';
-                    }
+                    this.getLanguageFlag(newFilm);
 
                     // change the rating system from 1 / 10 to 1 / 5
                     newFilm.vote_average = Math.floor(newFilm.vote_average / 2);
@@ -133,15 +136,7 @@ function init() {
                     let newTvSerie = {...tvSerie}
 
                     // Add flag key to every film object
-                    if (newTvSerie.original_language == 'en') {
-                        newTvSerie.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Flag_of_the_United_Kingdom.svg';
-
-                    } else if (newTvSerie.original_language == 'it') {
-                        newTvSerie.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg';
-
-                    } else {
-                        newTvSerie.languageFlag = 'https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg';
-                    }
+                    this.getLanguageFlag(newTvSerie);
 
                     // change the rating system from 1 / 10 to 1 / 5
                     newTvSerie.vote_average = Math.floor(newTvSerie.vote_average / 2);
